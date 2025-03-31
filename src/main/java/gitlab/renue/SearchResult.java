2579e9c31fccc89d0d51cee93f873d2fb36403cf
MrWhiteShapi <aigunovshapi@yandex.ru>
2025-03-31 12:01:36 +0300

    feat: реализован класс формирующий отчет

--- src/main/java/gitlab/renue/SearchResult.java
+++ src/main/java/gitlab/renue/SearchResult.java
@@ -1,11 +1,32 @@
 package gitlab.renue;
 
-public class SearchResult implements ReportElement{
-    public SearchResult(String request, Object result, long searchTime) {
+import java.util.List;
+
+public class SearchResult implements ReportElement {
+    private String search;
+    private List<Integer> result;
+    private long time;
+
+    public SearchResult(String search, List<Integer> result, long time) {
+        this.search = search;
+        this.result = result;
+        this.time = time;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public List<Integer> getResult() {
+        return result;
+    }
+
+    public long getTime() {
+        return time;
     }
 
     @Override
     public void accept(ReportVisitor visitor) {
-        
+        visitor.visit(this);
     }
 }
