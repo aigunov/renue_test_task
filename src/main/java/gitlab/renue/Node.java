a05cea6d6b8af5ca8217712f02343fcd7352eed5
MrWhiteShapi <aigunovshapi@yandex.ru>
2025-03-31 12:00:32 +0300

    feat: Реализовано префиксное древо

--- src/main/java/gitlab/renue/Node.java
+++ src/main/java/gitlab/renue/Node.java
@@ -1,4 +1,15 @@
 package gitlab.renue;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Node {
+    Map<Character, Node> children;
+    List<Integer> airportIds;
+
+    public Node() {
+        children = new HashMap<>();
+        airportIds = new ArrayList<>();
+    }
 }
--- src/main/java/gitlab/renue/Trie.java
+++ src/main/java/gitlab/renue/Trie.java
@@ -1,12 +1,47 @@
 package gitlab.renue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Trie {
-    public List<Integer> search(String request) {
-        return null;
+    private Node root;
+
+    public Trie() {
+        root = new Node();
     }
 
-    public void insert(String value, int id) {
+    public void insert(String value, int airportId) {
+        Node current = root;
+        for (char c : value.toCharArray()) {
+            current.children.putIfAbsent(c, new Node());
+            current = current.children.get(c);
+        }
+        current.airportIds.add(airportId);
+    }
+
+    public List<Integer> search(String prefix) {
+        Node current = root;
+        for (char c : prefix.toCharArray()) {
+            if (!current.children.containsKey(c)) {
+                return new ArrayList<>();
+            }
+            current = current.children.get(c);
+        }
+        return collectAirportIds(current);
+    }
+
+    private List<Integer> collectAirportIds(Node node) {
+        List<Integer> result = new ArrayList<>();
+        collectAirportIdsRecursive(node, result);
+        return result;
+    }
+
+    private void collectAirportIdsRecursive(Node node, List<Integer> result) {
+        if (node.airportIds != null && !node.airportIds.isEmpty()) {
+            result.addAll(node.airportIds);
+        }
+        for (Node child : node.children.values()) {
+            collectAirportIdsRecursive(child, result);
+        }
     }
 }
